fe4abb57fadc4929cbd55b2b6c6dbf314f308ca4
Владислав Крылов <krylov@sfedu.ru>
2021-10-21 10:39:12 +0300

    Add LaterInYear function in DateTime.pas

--- DateTime.pas
+++ DateTime.pas
@@ -1,4 +1,5 @@
-﻿function IsLeapYear(year: integer): boolean;
+﻿///Проверяет, является ли год високосным
+function IsLeapYear(year: integer): boolean;
 begin
   if (year mod 4 = 0) and (year mod 100 != 0) or (year mod 400 == 0):
     result := true;
@@ -6,6 +7,19 @@ else:
     result := false;
 end;
 
+///Получает два дня, проверяет, какой из них ближе к Новому году
+///(Возвращает 1, если первый, и 2, если второй
+function LaterInYear(firstDay, firstMonth, secondDay, secondMonth: integer): integer;
+begin
+  if firstMonth > secondMonth then
+    result := 1
+  else if firstMonth < secondMonth then
+    result := 2
+  else if firstDay > secondDay then
+    result := 1
+  else
+    result := 2;
+end;
 
 begin
   
