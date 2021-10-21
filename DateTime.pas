2feffa46366de0770b6beed41558d59ce0245aa9
Владислав Крылов <krylov@sfedu.ru>
2021-10-21 10:48:49 +0300

    Add Asserts in DateTime.pas

--- DateTime.pas
+++ DateTime.pas
@@ -1,6 +1,7 @@
 ﻿///Проверяет, является ли год високосным
 function IsLeapYear(year: integer): boolean;
 begin
+  Assert(year > 0);
   if (year mod 4 = 0) and (year mod 100 != 0) or (year mod 400 == 0):
     result := true;
 else:
@@ -21,11 +22,18 @@ begin
     result := 2;
 end;
 
-///Определяет количество дней в заданном году)
-function DaysInYear(year: integer): integer := IsLeapYear(year) ? 366 : 365;
+///Определяет количество дней в заданном году
+function DaysInYear(year: integer): integer;
+begin
+  Assert(year > 0);
+  result := IsLeapYear(year) ? 366 : 365;
+end;
 
+///Определяет количество дней между двумя годами включительно
 function DaysInYearRange(firstYear, secondYear: integer): integer;
 begin
+  Assert(firstYear > 0);
+  Assert(secondYear > 0);
   result := 0;
   for var i := firstYear to secondYear do
   begin
